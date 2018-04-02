8f151e3fa46aacd020a050c84a7ca5caaeef8f19
wdarvin <wesdarvin@gmail.com>
2018-04-02 17:55:26 -0400

    Thrift file for bank interface

---
+++ bank.thrift
@@ -0,0 +1,17 @@
+/**
+* This is a Thrift service definition, it lets you define functions for your
+* service.  Thrift will create an interface for compatible languages use use
+* for your client/server.
+**/
+
+service Bank extends shared.SharedService {
+
+    void ping(),
+
+    i32 get_balance(1:i32 account_num),
+
+    i32 withdraw(1:i32 account_num, 2:i32 amount) throws (1:InvalidOperation insufficient_funds),
+
+    i32 deposit(1:i32 account_num, 2:i32 amount),
+
+}
